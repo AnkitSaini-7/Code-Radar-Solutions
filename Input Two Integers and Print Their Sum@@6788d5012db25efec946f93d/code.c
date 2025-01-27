a10489f40d8d2674452a6cfd00bbf3e51f364f4b
AnkitSain7 <ankitsaini48698@gmail.com>
2025-01-27 17:41:16 +0530

    Solution to Input Two Integers and Print Their Sum

---
+++ Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+   int num1, num2, result;
+   cout<<"Enter num1"<<end1;
+   cin>> num1;
+   cout<<"Enter num2"<<end1;
+   cin>> num2; 
+   result = num1 +num2;
+    return 0;
+}
