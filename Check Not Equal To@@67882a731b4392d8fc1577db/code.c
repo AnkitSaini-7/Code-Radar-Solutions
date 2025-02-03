e23a4fc3ee6d6bd292ce268bda738cd831ced24c
Ankit Saini7 <ankitsaini48698@gmail.com>
2025-02-03 11:15:26 +0530

    Solution to Check Not Equal To

--- Check Not Equal To@@67882a731b4392d8fc1577db/code.c
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -10,9 +10,9 @@ int main() {
     scanf("%d", &b);
     if("a!=b"){
         printf("True");
-    }
-    else if(a == b){
-        printf("False");
-    }
+    
+         else if(a == b){
+             printf("False");
+    }}
     return 0;
 }
