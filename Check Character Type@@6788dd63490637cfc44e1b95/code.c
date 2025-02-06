116c4db5278a24f1449000dca2729b398069c3fd
Ankit Saini7 <ankitsaini48698@gmail.com>
2025-02-06 14:22:17 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    char b;
+    scanf("%d", &a);
+    scanf("%c", &b);
+    if()
+    return 0;
+}
