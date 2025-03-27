585c8774480ac574fb7c293ea52a05d52240623b
Ankit Saini <ankitsaini48698@gmail.com>
2025-03-27 14:11:43 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    int ranked[200000];
+    for (int i = 0; i < n; i++) scanf("%d", &ranked[i]);
+
+    scanf("%d", &m);
+    int player[200000];
+    for (int i = 0; i < m; i++) scanf("%d", &player[i]);
+
+    int result[200000];
+    
+    // Call trackPlayerRanks function (to be implemented by student)
+    trackPlayerRanks(ranked, n, player, m, result);
+
+    // Output rank after each game
+    for (int i = 0; i < m; i++) printf("%d\n", result[i]);
+
+    return 0;
+}
+
+// Implement the trackPlayerRanks function!
+// Hint: Create dense ranks first, then compare player scores efficiently.
